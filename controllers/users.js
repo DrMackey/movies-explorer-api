d562e19824c5f90ef02c5ca8eb019397a252ff46
DrMackey <DrMackey@yandex.ru>
2024-01-23 17:05:11 +0300

    dev: same site none

--- controllers/users.js
+++ controllers/users.js
@@ -105,7 +105,7 @@ module.exports.login = (req, res, next) => {
           .cookie('jwt', token, {
             maxage: 3600000 * 24 * 7,
             httpOnly: true,
-            sameSite: false,
+            sameSite: None,
           })
           .send({ message: 'Успешная авторизация.' });
       });
